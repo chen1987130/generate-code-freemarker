684c209846d3598e134dd89c397372287dce7871
chensheng@dtinsure.com <123456.com>
2018-11-12 15:52:05 +0800

    提交代码

--- src/main/java/auto/AutoProperties.java
+++ src/main/java/auto/AutoProperties.java
@@ -3,8 +3,8 @@ package auto;
 public class AutoProperties {
 
 	public static final String jdbcdriver = "com.mysql.jdbc.Driver";
-	public static final String jdbcurl = "jdbc:mysql://10.176.18.8:3306/market_test?characterEncoding=utf8&useSSL=true&useOldAliasMetadataBehavior=true";
-	public static final String username = "ins";
-	public static final String password = "ins123456";
+	public static final String jdbcurl = "";
+	public static final String username = "";
+	public static final String password = "";
 
 }
